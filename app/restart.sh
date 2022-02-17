1d965cc93688914f1854984ea98cd5de6b14cd9b
andres64372 <77692131+andres64372@users.noreply.github.com>
2022-02-17 17:14:37 -0500

    added api endpoint

--- app/restart.sh
+++ app/restart.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 cd /home/admin/RpServer/app
-sudo service nginx restart
 source venv/bin/activate
 git pull origin main
 sudo pkill gunicorn
 gunicorn --worker-class eventlet -w 1 main:app --daemon
+sudo service nginx restart
