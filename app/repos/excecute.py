ba4ff075d12725a950743868d9a38f3a70c05f5c
andres64372 <77692131+andres64372@users.noreply.github.com>
2022-05-14 15:08:02 -0500

    changed set steps

--- app/repos/excecute.py
+++ app/repos/excecute.py
@@ -13,23 +13,22 @@ def excecute(body,req_id):
         for device in devices:
             payload = {}
             id = device['id']
+            payload["ids"] = [id]
+            payload["status"] = "SUCCESS"
+            for excecute in excecution:
+                payload["states"] = excecute["params"]
+                payload["states"]["online"] = True
+                if excecute["command"] == "action.devices.commands.OnOff":
+                    mqtt.publish(f"{id}/OnOff","true" if excecute["params"]["on"] else "false")
+                    ref = db.reference(f'Devices/{id}/OnOff')
+                    ref.set({'on':excecute["params"]["on"]})
+                if excecute["command"] == "action.devices.commands.ColorAbsolute":
+                    mqtt.publish(f"{id}/Color",excecute["params"]["color"]["spectrumRGB"] if "spectrumRGB" in excecute["params"]["color"].keys() else 16777215)
+                    ref = db.reference(f'Devices/{id}/ColorSetting')
+                    ref.set({'color':excecute["params"]["color"]})
             ref = db.reference(f'Devices/{id}/Online')
             state = ref.get()['online']
-            if state:
-                payload["ids"] = [id]
-                payload["status"] = "SUCCESS"
-                for excecute in excecution:
-                    payload["states"] = excecute["params"]
-                    payload["states"]["online"] = True
-                    if excecute["command"] == "action.devices.commands.OnOff":
-                        mqtt.publish(f"{id}/OnOff","true" if excecute["params"]["on"] else "false")
-                        ref = db.reference(f'Devices/{id}/OnOff')
-                        ref.set({'on':excecute["params"]["on"]})
-                    if excecute["command"] == "action.devices.commands.ColorAbsolute":
-                        mqtt.publish(f"{id}/Color",excecute["params"]["color"]["spectrumRGB"] if "spectrumRGB" in excecute["params"]["color"].keys() else 16777215)
-                        ref = db.reference(f'Devices/{id}/ColorSetting')
-                        ref.set({'color':excecute["params"]["color"]})
-            else: 
+            if not state: 
                 payload = {
                     "ids": [id],
                     "status" : "OFFLINE",
